11dab9b5e13404cf096c0d9aee0afa22f9517eda
VladimirNull <tel89265725502@gmail.com>
2017-06-22 23:11:29 +0300

    0.000002

--- cellWalker.py
+++ cellWalker.py
@@ -3,6 +3,8 @@ import random
 import os
 import threading
 import time
+from bson.objectid import ObjectId
+import math
 
 class CellWalker(object):
     def __init__(self):
@@ -19,9 +21,12 @@ class CellWalker(object):
         return "there is nothing to see"
         
     def run(self):
+        i = 0
         while True:
             self.longwalk()
-            break;
+            i += 1;
+            time.sleep(0.1)
+            if i > 0: break;
             
     def refresh(self):
         pass
@@ -32,9 +37,9 @@ class CellWalker(object):
             main_data = {}
             for key in self.main_keys:
                 main_data[key] = document[key]
-            self.step(main_data,document['_id'])
+            self.step(main_data)
             
-    def step(self,main_data,id_cell):
+    def step(self,main_data):
         produce_shit = 0
         #print main_data
         #MINE
@@ -50,20 +55,61 @@ class CellWalker(object):
         #FACTORY
         gold = packets_pumped * main_data['MANA_GOLD']
         produce_shit += packets_pumped * main_data['MANA_SHIT']
+        #print produce_shit
         #FILTERS
         produce_shit -= 0
-        #NATURE
-        main_data['nature'] -= produce_shit
-        if main_data['nature']>=0:
-            main_data['people'] += main_data['nature']
-        else:
-            main_data['people'] -= main_data['nature']
+        #NATURE AND PEOPLE
+        main_data['shit'] = main_data['shit'] + produce_shit
+        tmp_nature = 0
+        if main_data['shit'] < (main_data['nature'] * 0.05):
+            #grow
+            tmp_nature = main_data['nature'] + (main_data['NATURE_LEVEL'] - main_data['nature'])/main_data['NATURE_LEVEL']
+        elif main_data['shit'] > (main_data['nature'] * 0.05) and main_data['shit'] < (main_data['nature'] * 0.26):
+            tmp_nature = main_data['nature']
+        elif main_data['shit'] > (main_data['nature'] * 0.25) and main_data['shit'] < (main_data['nature']):
+            #wither
+            tmp_nature = main_data['nature'] - (math.sqrt(main_data['shit']) + main_data['people']**(1/3.0))
+            if tmp_nature < 0 : tmp_nature = 0
+        elif main_data['shit'] > (main_data['nature']):
+            tmp_nature = 0
+        main_data['nature'] = tmp_nature
+        
+        children_of_nature = float(main_data['people']/((main_data['nature']+0.000001)*10))
+        print children_of_nature
+        if children_of_nature < 0.5:
+            print "free"
+            main_data['people'] += (main_data['people']/2)
+        elif children_of_nature > 0.49 and children_of_nature < 1:
+            print "norm"
+            main_data['people'] += math.sqrt(main_data['people'])
+        elif children_of_nature > 1:
+            print "die"
+            main_data['people']
+            main_data['people'] -= (main_data['people'] - main_data['nature']*10)
+            main_data['people']
+        elif children_of_nature == 0:
+            print "wind"
+            
         #REGENERATION MINE
         main_data['capacity'] += main_data['REGENERATION']
         if main_data['capacity'] > main_data['CAPACITY_LEVEL']:
             main_data['capacity'] = main_data['CAPACITY_LEVEL']
-        if str(id_cell)=='594abcdc77097b12bed7543a':
-            print main_data['people'],main_data['capacity'],main_data['nature']
+       
+        self.main_answer(main_data)
+        self.spread_shit(main_data)
+            
+    def main_answer(self,dict_data):      
+        self.db.map.update({"_id": ObjectId(dict_data['_id'])},{'$set':self.packing(dict_data,['people','capacity','nature'])})
+        
+    def spread_shit(self,dict_data):
+        self.db.map.update({"_id": ObjectId(dict_data['_id'])},{'$set':self.packing(dict_data,['shit'])})
+
+        
+    def packing(self,dict_data,variables):
+        tmp_dict = {}       
+        for key in variables:
+            tmp_dict[key] = dict_data[key]
+        return tmp_dict
         
         
         
--- create_map.py
+++ create_map.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 import random
 import os
+import time
 
 client = MongoClient()
 db = client.test
@@ -14,10 +15,10 @@ def create_map(len_x,len_y):
             NATURE_LEVEL = random.randrange(1,10)
             PRESSURE = random.randrange(1,10)
             MANA_GOLD = round(float(random.randrange(51,95))/100,2)
-            MANA_SHIT = round((1 -MANA_GOLD),2)
+            MANA_SHIT = round((1 - MANA_GOLD),2)
             REGENERATION = random.randrange(1,10)
             CAPACITY_LEVEL = random.randrange(10,1000)
-            player_id = 0000
+            player_id = None
             people = 100
             shit = 0
             capacity = CAPACITY_LEVEL
@@ -33,11 +34,17 @@ def create_map(len_x,len_y):
 def read_map():
     cursor = db.map.find()
     for document in cursor:
-        print(document['_id'])
+        print document
+        if str(document['_id']) == '594c221f77097b2878b9c859':
+            print document
 def delete_map():
     cursor = db.map.drop()
-        
-
-create_map(2,5)
-#read_map()
-#delete_map()
+    
+read_map()  
+i = 0
+while False:
+    read_map()
+    i += 1;
+    time.sleep(0.5)
+    os.system('clear')
+    if i > 100: break;
